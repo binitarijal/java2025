0dce356bcfc58c9ac61e544f423971ff9faf124a
Binita rijal <rijalbinita01@gmail.com>
2025-09-17 22:07:16 +0545

    basics file

---
+++ Basics.java
@@ -0,0 +1,13 @@
+
+public class Basics {
+    public static void main(String[] args) {
+// Variable declaration and initialization
+        int a= 23;
+        String s= "hello";
+        double d= 4.5;
+         System.out.println(a);
+         System.out.println(s);
+         System.out.println(d);
+    }
+    }
+    
