c11585c2f50c75c81db4f293d10ffb8a2ee637e3
Binita rijal <rijalbinita01@gmail.com>
2025-09-21 21:49:20 +0545

    linked list

--- Linkedlist.java
+++ Linkedlist.java
@@ -1,28 +1,28 @@
 public class Linkedlist {
-class Node {
-    int data;
-    Node next;
-    Node(int data) { this.data = data; this.next = null; }
-}
 
-Node head = null;
+    public static class Node {
+        int data;
+        Node next;
 
- void insertAtBeginning(int val) {
-    Node newNode = new Node(val);
-    newNode.next = head;
-    head = newNode;
-}
-public static void main(String[] args) {
-    Linkedlist list = new Linkedlist();
-    list.insertAtBeginning(10);
-    list.insertAtBeginning(20);
-    list.insertAtBeginning(30);
-
-    Node current = list.head;
-    while (current != null) {
-        System.out.print(current.data + " -> ");
-        current = current.next;
+        Node(int data) {
+            this.data = data;
+        
+        }
     }
-    System.out.println("null");
+public static void main(String[] args) {
+    Node a=    new Node(10);
+    Node b=    new Node(20);
+    Node c=    new Node(30);
+    Node d=    new Node(40);
+
+    a.next=b;
+    b.next=c;
+    c.next=d;
+    d.next=null;
+    System.out.println(a.data);
+    System.out.println(b.data);
+    System.out.println(c.data);
+    System.out.println(d.data);
+
 }
 }
