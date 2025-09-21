c370ef0385c3a4b1a1db3dae02de4879e5d463fa
Binita rijal <rijalbinita01@gmail.com>
2025-09-21 21:57:15 +0545

    to display all

--- Linkedlist.java
+++ Linkedlist.java
@@ -24,9 +24,16 @@ public static void main(String[] args) {
     // System.out.println(c.data);
     // System.out.println(d.data);
 
-    System.out.println(a.data);
-    System.out.println(a.next.data);
-    System.out.println(a.next.next.data);
+    // System.out.println(a.data);
+    // System.out.println(a.next.data);
+    // System.out.println(a.next.next.data);
+
+    Node temp=a;
+    while(temp!=null){
+        System.out.println(temp.data);
+        temp=temp.next;
+}
+
 
 }
 }
