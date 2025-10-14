fba9a0399e74e42917affc2df2dde5d6a01faf19
Binita rijal <rijalbinita01@gmail.com>
2025-10-14 16:39:28 +0545

    question solve

--- Array.java
+++ Array.java
@@ -22,5 +22,25 @@ public class Array {
             }
         }
         System.out.println("maximum value is "+max);
+
+
+
+//array reverse
+int a[]={30,40,50,70};
+int start=0;
+int end=a.length-1;
+
+while(start<end){
+    int temp=a[start];
+    a[start]=a[end];
+    a[end]=temp;
+    start++;
+    end--;
+}
+// Optionally, print the reversed array
+for(int i=0; i<a.length; i++){
+    System.out.print(a[i] + " ");
+}
+
     }
 }
