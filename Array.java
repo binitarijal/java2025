452e09713f9e7e817dde93534d9c81ab6f0eeabd
Binita rijal <rijalbinita01@gmail.com>
2025-10-16 20:34:38 +0545

    linkedin

--- Array.java
+++ Array.java
@@ -13,34 +13,77 @@ public class Array {
 
         //maximum in array
 
-        int arr[]={10,20,30,40,50};
-        int max= arr[0];
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]>max){
-                max=arr[i];
+        // int arr[]={10,20,30,40,50};
+        // int max= arr[0];
+        // for(int i=0;i<arr.length;i++){
+        //     if(arr[i]>max){
+        //         max=arr[i];
                 
-            }
-        }
-        System.out.println("maximum value is "+max);
+        //     }
+        // }
+        // System.out.println("maximum value is "+max);
 
 
 
 //array reverse
-int a[]={30,40,50,70};
-int start=0;
-int end=a.length-1;
+// int a[]={30,40,50,70};
+// int start=0;
+// int end=a.length-1;
 
-while(start<end){
-    int temp=a[start];
-    a[start]=a[end];
-    a[end]=temp;
-    start++;
-    end--;
-}
-// Optionally, print the reversed array
-for(int i=0; i<a.length; i++){
-    System.out.print(a[i] + " ");
-}
+// while(start<end){
+//     int temp=a[start];
+//     a[start]=a[end];
+//     a[end]=temp;
+//     start++;
+//     end--;
+// }
+// // Optionally, print the reversed array
+// for(int i=0; i<a.length; i++){
+//     System.out.print(a[i] + " ");
+// }
+
+
+
+// array target search
+
+// int target =50;
+// int arr1[]={10,20,30,40,50};
+// for (int i=0;i<arr1.length;i++){
+//     if(arr1[i]==target){
+//         System.out.println("target found at "+i);
+//         break;
+//     }
+// }
+
+
+
+//
+
+
+int arr2[]={10,20,30,40,50};
+int target=40;
+
+int start=0;
+int end=arr2.length-1;
+
+while(start<=end){
+    int mid=(start+end)/2;
+    if(arr2[mid]==target){
+        System.out.println("target found at "+mid);
+        break;
+    }
+    else if(arr2[mid]<target){
+        start=mid+1;
 
     }
+    else{
+        end=mid-1;
+    }
 }
+    }
+}
+
+
+
+
+
--- Linkedlist.java
+++ Linkedlist.java
@@ -1,50 +1,44 @@
-public class Linkedlist {
 
-    public static class Node {
-        int data;
-        Node next;
+public class Linkedlist{
 
-        Node(int data) {
-            this.data = data;
-        
+public static class Node{
+    int data;
+    Node next;
+
+    Node(int dataa){
+        this.data=dataa;
+    }
+    public static void printt(Node head){
+        while(head != null){
+            System.out.println(head.data);
+            head=head.next;
         }
     }
-public static void main(String[] args) {
-    Node a=    new Node(10);
-    Node b=    new Node(20);
-    Node c=    new Node(30);
-    Node d=    new Node(40);
-
-    a.next=b;
-    b.next=c;
-    c.next=d;
-    d.next=null;
-    // System.out.println(a.data);
-    // System.out.println(b.data);
-    // System.out.println(c.data);
-    // System.out.println(d.data);
-
-    // System.out.println(a.data);
-    // System.out.println(a.next.data);
-    // System.out.println(a.next.next.data);
-
-
-
-    System.out.println(a.data);
-    System.out.println(b.data);
-    System.out.println(c.data);
-    System.out.println(d.data);
-
-    System.out.println(a.data);
-    System.out.println(a.next.data);
-    System.out.println(a.next.next.data);
-
-    Node temp=a;
-    while(temp!=null){
-        System.out.println(temp.data);
-        temp=temp.next;
-}
-
 
 }
+    public static void main(String[] args) {
+        Node a=new Node(3);
+        Node b=new Node(4);
+        Node c=new Node(5);
+        Node d=new Node(6);
+        Node e=new Node(8);
+
+        a.next=b;
+        b.next=c;
+        c.next=d;
+        d.next=e;
+        e.next=null;
+        // System.out.println(a.data);
+        // System.out.println(a.next.data);
+        // System.out.println(c.next.data);
+        // System.out.println(d.next.data);
+        // System.out.println(e.data);
+        // Node temp=a;
+        // while(temp != null){
+        //     System.out.println(temp.data);
+        //     temp=temp.next;
+        //}
+
+       Node.printt(a);
+    }
 }
